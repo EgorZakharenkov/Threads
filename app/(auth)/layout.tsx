f50736ca96b21f33f0a0e5da85c78d4872029b84
EgorZakharenkov <egorboom2017@gmail.com>
2023-08-11 21:11:13 +0300

    First commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+export const metadata = {
+  title: "Threads",
+  description: "A NextJs13 metadata Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
