52e0f12df9451492e453dcb92e5a37cb587c7d29
EgorZakharenkov <egorboom2017@gmail.com>
2023-08-18 18:38:24 +0300

    new files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 //app/page.tsx
 import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Home() {
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
-      <UserButton afterSignOutUrl="/" />
+      <UserButton
+        appearance={{
+          baseTheme: dark,
+        }}
+        afterSignOutUrl="/"
+      />
     </div>
   );
 }
--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { usePathname } from "next/navigation";
+
 function Bottombar() {
-  return <h1>BottomBar</h1>;
+  const pathName = usePathname();
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathName.includes(link.route) && link.route.length > 1) ||
+            pathName === link.route;
+          return (
+            <div>
+              <Link
+                className={`bottombar_link ${isActive && "bg-primary-500"}`}
+                href={link.route}
+              >
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                />
+                <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                  {link.label.split(/\s+/)[0]}
+                </p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
